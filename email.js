4d3ac92a9ebb6600818559463a6c46453224993a
AnaLuiza <aninha141108@gmail.com>
2024-07-26 00:33:44 -0300

    adiciona comentários em código para melhor entendimento

--- email.js
+++ email.js
@@ -1,13 +1,19 @@
 // CÓDIGO DE VALIDAÇÃO DO EMAIL
 // ------------------------------------------
+// Obtém o valor do campo de e-mail do primeiro formulário da página
 function checarEmail(){
-    if(document.forms[0].email.value == "" ||
+    // Verifica se o campo de e-mail está vazio ou se não contém o caractere "@"
+    if(document.forms[0].email.value == "" ||    
        document.forms[0].email.value.indexOf('@') == -1||
        document.forms[0].email.value.indexOf('') == -1){
+        // Exibe um alerta informando que o e-mail é inválido
        alert("Por favor, informe um e-mail valido");
+       // Impede o envio do formulário (retorna falso)
        return false;
     }else{
+        // Se o e-mail for válido, exibe o valor do e-mail em um elemento com o id "email"
         document.getElementById('email').innerHTML = document.forms[0].email.value;
+        // Exibe um alerta confirmando o e-mail
         alert("Email confirmado");
        
     }
